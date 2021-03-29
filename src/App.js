ddee2d196a868bf7006f8be3810197e7a4c0b0bd
kumar indala <91934@DESKTOP-VV8OPAL.micelio.local>
2021-03-29 16:51:53 +0530

    kumar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Header from "./Layouts/Components/Header";
+import SideMenu from "./Layouts/Components/SideMenu";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <SideMenu />
+<Dashboard />
     </div>
   );
 }
