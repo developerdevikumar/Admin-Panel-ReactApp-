f4ed9394589ec7201e180f0db83eb6b9b478f9ee
kumar indala <91934@DESKTOP-VV8OPAL.micelio.local>
2021-03-30 10:51:37 +0530

    'dk'

--- src/Screens/Dashboard/Components/WidgetLst.js
+++ src/Screens/Dashboard/Components/WidgetLst.js
@@ -2,27 +2,65 @@ import React from 'react';
 import WidgetItem from './WidgetItem';
 function WidgetLst() {
     return (
-        <div className="content-wrapper">
-<div className="content-header">
-      <div className="container-fluid">
-        <div className="row mb-2">
-          <div className="col-sm-6">
-            <h1 className="m-0">Dashboard</h1>
-          </div>
-          <div className="col-sm-6">
-            <ol className="breadcrumb float-sm-right">
-              <li className="breadcrumb-item"><a href="#">Home</a></li>
-              <li className="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div>
+
+        <section className="content">
+        <div className="container-fluid">
+      <div className="row">
+      <div className="col-lg-3 col-6">
+      <div className="small-box bg-info">
+      <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+<WidgetItem  text="kumar" text="yjhjj" text="fghgj"/>
+
+</div>
+</div>
+<div className="col-lg-3 col-6">
+
+<div className="small-box bg-success">
+<div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+
+<WidgetItem text="djjd" text="udufj" text="hkch" />
+
+
+
+</div>
+</div>
+<div className="col-lg-3 col-6">
+
+<div className="small-box bg-warning">
+<div className="icon">
+    <i className="ion ion-person-add"></i>
+  </div>
+
+
+<WidgetItem text="djjd" text="udufj" text="hkch" />
+
+
+
+</div>
+</div>
+<div className="col-lg-3 col-6">
+<div className="small-box bg-danger">
+<div className="icon">
+    <i className="ion ion-pie-graph"></i>
+  </div>
+
+
+<WidgetItem text="djjd" text="udufj" text="hkch" />
+
+
+
+</div>
+</div>
+
+
+</div>
+
         </div>
-      </div>
-
-    </div>
-
-<WidgetItem />
-
-        </div>
+        </section>
     )
 }
 
