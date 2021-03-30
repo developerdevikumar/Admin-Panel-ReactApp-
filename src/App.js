c988910b069a9af00311d81ac539d6727817d9a3
kumar indala <91934@DESKTOP-VV8OPAL.micelio.local>
2021-03-30 09:59:58 +0530

    'dk'

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import Dashboard from "./Screens/Dashboard/Dashboard";
 import Header from "./Layouts/Components/Header";
 import SideMenu from "./Layouts/Components/SideMenu";
 
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+import Widgets from "./Screens/Widgets/Widgets";
 
 function App() {
   return (
+    <Router>
     <div>
       <Header />
+      <Switch>
+
+      <Route exact path="/">
+                <Redirect to="/dashboard" />
+            </Route>
+            <Route exact path='/dashboard' component={Dashboard} />
+            <Route  path="/Widgets" component={Widgets} />x
+      </Switch>
       <SideMenu />
-<Dashboard />
+
     </div>
+    </Router>
   );
 }
 
---
+++ src/Screens/Widgets/Widgets.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Widgets() {
+    return (
+             <div class="content-wrapper">
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Widgets</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Widgets</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+        </div>
+    )
+}
+
+export default Widgets
